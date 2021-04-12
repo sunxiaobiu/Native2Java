9e5511fbf5dcb070e091796eeef4f90feb009f75
seanonymousreview <se.anonymous.review@gmail.com>
2021-04-12 22:51:28 +1000

    Fix bug:getAllReachableMethods

--- src/main/java/edu/monash/Main.java
+++ src/main/java/edu/monash/Main.java
@@ -67,6 +67,7 @@ public class Main {
         Options.v().set_process_dir(Collections.singletonList(apkPath));
         Options.v().set_whole_program(true);
         Options.v().set_allow_phantom_refs(true);
+        Options.v().set_allow_phantom_elms(true);
         Options.v().set_keep_line_number(true);
         Options.v().set_ignore_resolution_errors(true);
         Options.v().set_output_dir(outputPath);
